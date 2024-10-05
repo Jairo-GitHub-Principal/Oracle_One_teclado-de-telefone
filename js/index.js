d065ce539a58dd795b64380826aa37fa5d31279c
Jairo-GitHub-Principal <jairocesa@outlook.com.br>
2024-10-04 22:18:16 -0300

    incerimos comentarios no js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-const listaDeTeclas = document.querySelectorAll('input[type=button]');
-const inputTel = document.querySelector('input[type=tel]');
+const listaDeTeclas = document.querySelectorAll('input[type=button]'); // seleciona as teclas do teclado
+const inputTel = document.querySelector('input[type=tel]'); // seleciona a entrada de dados, onde o numero da tecla vai aparedcer quando o mesmo for clicado
 
-for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+for (let indice = 0; indice < listaDeTeclas.length; indice++) { // laço de repetição
   
-  const tecla = listaDeTeclas[indice];
+  const tecla = listaDeTeclas[indice]; /*** a variavel listadetecla nela esta armazenada um array de tecla 
+                                            ou seja, todas as teclas input que tem o atributo type="button" ou input[type=tel]
+                                            e todo dado em uma  posição do array é acessado atraves do seu indice
+                                            sendo assim o laço de repetição incrementa esse indice e a cada repecição
+                                            sera armazenado no cont tecla uma tecla de um indice diferente */
   
-  tecla.addEventListener("click",function () {
-    inputTel.value = inputTel.value + tecla.value;
+  tecla.addEventListener("click",function () { /**aqui o addeventlistner implementa um evento de click para a tecla selecionado,
+                                                de um indice especifico */
+    inputTel.value = inputTel.value + tecla.value; /** atribui a entrada de dados onde é incerido o numero digitado 
+                                                    o valor da tecla que foi precionada */
   })
 }
